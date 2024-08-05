61b2081652645a600024535756c2bdc0733dc46f
Konstantin Ahremchuk <ahremchuk.kostya@gmail.com>
2024-08-05 07:04:17 +0300

    feat: realize reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  const nString = n.toString().split('');
+  if (nString[0] == '-') {
+    nString.shift();
+    return Number(nString.reverse().join(''));
+  } else {
+    return Number(nString.reverse().join(''));
+  }
 }
